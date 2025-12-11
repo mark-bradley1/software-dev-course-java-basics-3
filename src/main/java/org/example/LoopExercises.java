c6067d06dee2b718c3df0f9a7eef13301fc46c16
mark-bradley1 <mbradle15@gmail.com>
2025-12-10 20:34:17 -0600

    Completed assignment

--- src/main/java/org/example/LoopExercises.java
+++ src/main/java/org/example/LoopExercises.java
@@ -24,6 +24,4 @@ public class LoopExercises {
         return total;
     }
 }
-// Replace the line below with code that returns the sum of the numbers from 1 to n
-// but stops adding when the sum is even
-// (use a while loop with a sum variable and a counter variable)
+
